7c0d7b52f1dfdb53e17d4bb92f5f8b2b77f6823e
Boris <bbogdanovic167@gmail.com>
2025-10-02 12:46:52 +0200

    restrict-messages-to-conversations

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,11 +2,38 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Message;
+use App\Models\User;
 use App\Events\MessageSent;
 use App\Http\Requests\MessageRequest;
+use App\Http\Requests\FetchMessageRequest;
+use Illuminate\Support\Facades\Auth; 
 
 class ChatController extends Controller
 {
+
+public function fetchMessages (FetchMessageRequest $request ){
+
+  $contact= User::findOrFail($request->contact_id);
+
+   $messages = Message::where(function($q) use ($request) {
+        $q->where('from_id', Auth::id())
+          ->where('to_id', $request->contact_id);
+    })
+    ->orWhere(function($q) use ($request) {
+        $q->where('from_id', $request->contact_id)
+          ->where('to_id', Auth::id());
+    })
+    ->orderBy('created_at', 'asc')
+    ->get();
+
+
+
+     return response()->json([
+        'status' => 'success',
+        'contact'=>$contact,
+        'messages'=>$messages
+     ]);
+} 
 public function sendMessage(MessageRequest $request){
 
         $message = Message::create([
--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,18 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ChatController;
 
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
 
 Route::middleware('auth:sanctum')->group(function () {
-  Route::post('/logout', [AuthController::class, 'logout']);
+  Route::post('logout', [AuthController::class, 'logout']);
   Route::patch('user/settings', [UserController::class, 'settings']);
   Route::get('users', [UserController::class, 'fetchUsers']);
-  Route::post('/user/update-image', [UserController::class, 'updateImage']);
-  Route::post('/send-message', [ChatController::class, 'sendMessage']);
+  Route::post('user/update-image', [UserController::class, 'updateImage']);
+  Route::post('send-message', [ChatController::class, 'sendMessage']);
+  Route::get('fetch-messages',[ChatController::class,'fetchMessages']);
 });
+
+
