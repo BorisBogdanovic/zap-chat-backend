07ec6ffca14139ff0cd45251c1de6fd307c2a1d7
Boris <bbogdanovic167@gmail.com>
2025-10-05 22:47:38 +0200

    channel fixing

--- routes/channels.php
+++ routes/channels.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Broadcast;
 
 
 Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
+
 Broadcast::channel('chat.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-
 
 Broadcast::channel('online', function ($user) {
    return $user->toArray();
