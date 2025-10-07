f7a79b071565f26919fa43dd8e2616ac6e10a4ea
Boris <bbogdanovic167@gmail.com>
2025-10-07 21:05:35 +0200

    fix: avatar upload cleanup and filename sanitization

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserSettingsRequest;
 use App\Http\Requests\UpdateUserImageRequest;
 use App\Http\Requests\UserSearchRequest;
-
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Storage;
 use App\Facades\UserFacade;
 use App\Services\UserService;
 use Illuminate\Http\JsonResponse;
@@ -57,38 +54,31 @@ public function settings(UserSettingsRequest $request): JsonResponse
         ], 200);
 }
 //////////////////////////////////////////////////////////////////////////////UPDATE USER IMAGE
-public function updateImage(UpdateUserImageRequest $request)
+public function updateImage(UpdateUserImageRequest $request): JsonResponse
 {
     $user = auth()->user();
-    if(!$request->hasFile('avatar')) {
+
+    if (!$request->hasFile('avatar')) {
         return response()->json([
             'status' => 'error',
             'message' => 'No avatar file uploaded.'
         ], 422);
     }
-    $file = $request->file('avatar');
-    $timestamp = now()->timestamp;
-    $ext = $file->getClientOriginalExtension();
-    $fileName = 'avatar_' . $user->id . '_' . $user->username . '_' . $timestamp . '.' . $ext;
-    $path = $file->storeAs('images/avatars', $fileName, 'public');
-    if($user->image_path !== 'images/default.png' && Storage::disk('public')->exists($user->image_path)){
-        Storage::disk('public')->delete($user->image_path);
-    }
-    $user->image_path = $path;
-    $user->save();
 
-    return response()->json([
+    $updatedUser=UserFacade::updateAvatar($user, $request->file('avatar'));
+
+     return response()->json([
         'status'  => 'success',
         'message' => 'Avatar updated successfully!',
         'data'    => [
-            'id'         => $user->id,
-            'name'       => $user->name,
-            'last_name'  => $user->last_name,
-            'username'   => $user->username,
-            'image_path' => $user->image_path,
-            'avatar_url' => asset('storage/' . $user->image_path)
+            'id'         => $updatedUser->id,
+            'name'       => $updatedUser->name,
+            'last_name'  => $updatedUser->last_name,
+            'username'   => $updatedUser->username,
+            'image_path' => $updatedUser->image_path,
+            'avatar_url' => asset('storage/' . $updatedUser->image_path)
         ],
-    ]);
+    ],200);
 }
 
 
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,6 +5,10 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth; 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
+
 
 class UserService
 {
@@ -26,7 +30,7 @@ public function fetchUsers(?string $search = null): Collection
         return $query->get(['id', 'name', 'last_name', 'email', 'image_path', 'username']);
 }  
 
-
+//////////////////////////////////////////////////////////////////////////////EDIT USER SETTIGS SERVICES
 public function updateSettings(User $user, array $data): User
 {
        
@@ -35,6 +39,27 @@ public function updateSettings(User $user, array $data): User
         }
 
         $user->fill($data);
+        $user->save();
+
+        return $user;
+}
+//////////////////////////////////////////////////////////////////////////////EDIT USER AVATAR SERVICES
+public function updateAvatar(User $user, UploadedFile $file): User
+{
+        $timestamp = now()->timestamp;
+        $ext = $file->getClientOriginalExtension();
+        $slugUsername = Str::slug($user->username);
+        $fileName = 'avatar_' . $user->id . '_' . $slugUsername . '_' . $timestamp . '.' . $ext;
+
+      
+        $path = $file->storeAs('images/avatars', $fileName, 'public');
+
+       
+        if ($user->image_path !== 'images/default.png' && Storage::disk('public')->exists($user->image_path)) {
+            Storage::disk('public')->delete($user->image_path);
+        }
+
+        $user->image_path = $path;
         $user->save();
 
         return $user;
