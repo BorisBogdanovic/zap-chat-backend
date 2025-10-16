80934a8023e53fe0d8cce42c4af09abbe87b62d4
Boris <bbogdanovic167@gmail.com>
2025-10-16 11:35:07 +0200

    fix

--- config/cors.php
+++ config/cors.php
@@ -1,38 +1,11 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Ovde se podešava komunikacija između tvog frontenda (React/Vite)
-    | i Laravel backenda (API i broadcasting kanali).
-    |
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
-
-    
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-   
-    'allowed_origins' => ['http://localhost:5173'],
-
-   
-    'allowed_origins_patterns' => [],
-
-    
+    'allowed_origins' => ['*'], 
     'allowed_headers' => ['*'],
-
-    
     'exposed_headers' => [],
-
-    
     'max_age' => 0,
-
-    
     'supports_credentials' => true,
-
 ];
